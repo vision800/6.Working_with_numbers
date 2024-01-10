0122133ac4a5502f4416305c4a7d44c5738d17bf
vision800 <netshokotshokovision112@gmail.com>
2024-01-10 09:59:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Working_with_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //printing a number
+            Console.WriteLine(20);
+
+            //adding two numbers
+            Console.WriteLine(30 + 90);
+
+            //subtracting two numbers
+            Console.WriteLine(30 + 10);
+
+            //multiplying two numbers
+            Console.WriteLine(3*9);
+
+            //dividing two numbers
+            Console.WriteLine(30/10);
+
+            //modulus of two numbers
+            Console.WriteLine(11%5);
+
+            //adding multiple operations
+            Console.WriteLine(5+8*7);
+
+            int num = 6;
+            Console.WriteLine("before increment : "+num);
+            //increment number by 1
+            num++;
+            //
+            Console.WriteLine("After increment : " + num);
+
+            //Math functions
+
+            //absolte functions gets positive numbers only
+            Console.WriteLine(Math.Abs(-4));
+
+            //power functions get the value of number to the power of another number
+            Console.WriteLine(Math.Pow(2, 4));
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("6.Working_with_numbers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Standard Bank of South Africa")]
+[assembly: AssemblyProduct("6.Working_with_numbers")]
+[assembly: AssemblyCopyright("Copyright © Standard Bank of South Africa 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6e408e19-4f36-46a7-a0e7-144f62d88cde")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
